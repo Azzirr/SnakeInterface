716bdd66df1967fd944110c6c2a45b506d0bee57
Azzirr <arek.sliwowski37@gmail.com>
2023-01-20 14:37:40 +0100

    Added sort funcionality to game history

--- snake-game/src/app/play/play.component.ts
+++ snake-game/src/app/play/play.component.ts
@@ -11,26 +11,38 @@ export class PlayComponent implements OnInit {
   playing: boolean = true;
   gameStatus: string = 'Ready to go';
   gameHistory: Array<string> = [];
+  points: number = 0;
+  sort: Array<any> = [{description: 'From earliest to latest'}, {description: 'From latest to earliest'}];
+  defaultSortValue: string = 'From earliest to latest'; //used for sort table
+
 
   public gameStatusStart(){
     this.gameStatus = 'Game is running'
-    this.gameHistory.push(this.gameStatus)
+    this.gameHistory.push(`Second ${this.timer}: ` + this.gameStatus)
   }
   public gameStatusEnd(){
     this.gameStatus = 'Game over'
-    this.gameHistory.push(this.gameStatus)
+    this.gameHistory.push(`Second ${this.timer}: ` + this.gameStatus)
   }
   public gameStatusReset(){
     this.gameStatus = 'Ready to go'
-    this.gameHistory.push('Game restarted and ready to go')
+    this.gameHistory.push(`Second ${this.timer}: ` + 'Game restarted and ready to go')
   }
   public gameStatusPaused(){
     this.gameStatus = 'Game is paused'
-    this.gameHistory.push(this.gameStatus)
+    this.gameHistory.push(`Second ${this.timer}: ` + this.gameStatus)
+    console.log(this.gameHistory)
   }
   public exitButton(){
     this.playing = false;
   }
+  public sortTable(value: any){
+    this.startSort(value);
+  }
+  public startSort(value: any){
+      this.gameHistory.reverse();
+  }
+
 
   @Input() name: string = '';
   constructor() { }
