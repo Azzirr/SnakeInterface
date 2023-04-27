6bcae4c02fae3d68c1fe3bfd00d97642ca941bbc
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-04-27 19:01:59 +0200

    Reactive form

--- snake-game/src/app/login-form/login-form.component.ts
+++ snake-game/src/app/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, Input, ElementRef, Output, EventEmitter, Injectable } from '@angular/core';
 import { DataService } from '../Services/data.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'login-form',
   templateUrl: './login-form.component.html',
@@ -7,21 +8,26 @@ import { DataService } from '../Services/data.service';
 })
 
 export class LoginFormComponent implements OnInit {
-  constructor(private data: DataService){
+  constructor(
+    private data: DataService,
+    private formBuilder: FormBuilder
+    ){
 
   }
   updateName(name: string){
     this.data.updateData(name);
   }
+  loginForm!:FormGroup
   tokenType: string = 'password';
   tokenShown: boolean = false;
   name: string = '';
   displayValue: string = '';
   token: string = '';
-  @Output() eventTask = new EventEmitter<string>();
-  insertName(task: string){
-    this.eventTask.emit(task)
-  }
+  submitted: boolean = false;
+  // @Output() eventTask = new EventEmitter<string>();
+  // insertName(task: string){
+  //   this.eventTask.emit(task)
+  // }
   hidePassword(){
     if(this.tokenShown){
       this.tokenShown = false;
@@ -30,8 +36,8 @@ export class LoginFormComponent implements OnInit {
       this.tokenShown = true;
       this.tokenType = 'text'
     }
-    console.log(this.name)
   }
+  
   @ViewChild('whenEmpty')
   whenEmpty!: ElementRef;
 
@@ -42,7 +48,22 @@ export class LoginFormComponent implements OnInit {
   getToken(value: string){
     this.token = value;
   }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.loginForm.invalid){
+      return;
+    } else{
+      alert('Success!');
+    }
+  }
+
   ngOnInit(): void {
+    //validations
+    this.loginForm = this.formBuilder.group({
+      userName:['', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]],
+      password:['', [Validators.required, Validators.minLength(4)]]
+    })
   }
 
 }
